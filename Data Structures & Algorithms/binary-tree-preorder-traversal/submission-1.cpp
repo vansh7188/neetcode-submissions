5e65be45e2de12ca1b72de5388c528d4c47c04be
vansh_30_ <167352262+vansh7188@users.noreply.github.com>
2026-08-19 03:14:56 +0530

    Add: binary-tree-preorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(root==nullptr)return ans;
+        //now declare a stack 
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode*curr=st.top();
+            st.pop();
+            ans.push_back(curr->val);
+            if(curr->right !=nullptr)st.push(curr->right);
+            if(curr->left !=nullptr)st.push(curr->left);
+        }return ans;
+
+    }
+};
